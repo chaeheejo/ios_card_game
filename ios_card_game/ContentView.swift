dafc2aee66df67693cc71f98356802778b7c3080
chaeheejo <60cogml@gmail.com>
2023-01-12 18:59:17 +0900

    change user interface

--- ios_card_game/ContentView.swift
+++ ios_card_game/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello!").padding()
+            .background(Color.blue.blur(radius: 3.0))
     }
 }
 
