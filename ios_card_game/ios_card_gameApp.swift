5ad073c08c09dbd8473d73e3ab1d8e18ec56c118
chaeheejo <60cogml@gmail.com>
2023-01-12 17:59:38 +0900

    Initial Commit

---
+++ ios_card_game/ios_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_card_gameApp.swift
+//  ios_card_game
+//
+//  Created by chaehee on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct ios_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
