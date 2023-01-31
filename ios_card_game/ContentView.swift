ff4e8c149adbd29a6f14f447898d1e57bb5b496b
chaeheejo <60cogml@gmail.com>
2023-01-31 22:01:07 +0900

    success to show random card and count score

--- ios_card_game/ContentView.swift
+++ ios_card_game/ContentView.swift
@@ -8,50 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //game state
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         ZStack{
             Image("background")
                 .ignoresSafeArea()
+            
             VStack{
                 Spacer()
+                
                 Image("logo")
+                
                 Spacer()
                 
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 
                 Spacer()
-                Image("dealbutton")
+                
+                Button(action: {
+                    
+                    //generate a random number 2 ~ 13
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    //update the card and score
+                    playerCard = "card"+String(playerRand)
+                    cpuCard = "card"+String(cpuRand)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
                 Spacer()
                 
                 HStack{
                     Spacer()
+                    
                     VStack{
                         Text("Player")
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
+                    
                     Spacer()
+                    
                     VStack{
                         Text("CPU")
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
+                    
                     Spacer()
                 }
+                
                 Spacer()
             }
         }
